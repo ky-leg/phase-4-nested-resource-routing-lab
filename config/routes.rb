228c34c8fc2b780650476c2633783e69cfd6bd81
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-05-05 14:59:33 -0400

    commit tree 84704f34d3e3ca2d11841f65c92463d9adf3e437

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :items, only: [:index, :create] do
-    resources :users, only: [:show]
+  resources :items, only: [:index, :show] do
   end
 
   resources :users, only: [:index, :show] do
-    resources :items, only: [:index]
+    resources :items, only: [:index, :show, :create]
   end
 
 end
