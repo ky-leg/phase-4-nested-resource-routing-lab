0f1681dbe877325dc7bb1948279de836fb5b3a9c
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-05-05 10:04:31 -0400

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,15 @@ class ItemsController < ApplicationController
     render json: items, include: :user
   end
 
+  def create 
+    item = Item.create(item_params)
+    render json: item, status :created
+  end
+
+  private 
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+
+  resources :items, only: [:index, :create] do
+    resources :users, only: [:show]
+  end
+
+  resources :users, only: [:index, :show] do
+    resources :items, only: [:index]
+  end
+
 end
